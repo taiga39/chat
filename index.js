d9c6c32cec8fe8c35478c3b7a2b8354ee8f38d6b
taiga39 <ishikawa.tigers@gmail.com>
2020-02-11 00:08:04 +0900

    index修正

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-app.use(log.access.add); // accessログに記録するためのミドルウェア
+const path=require('path');
+const express=require('express');
+const app=express();
+
+
+app.set('port', process.env.PORT || 5050);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), ()=>{ console.log("Node app is running at localhost:" + app.get('port')); });
+
+app.use(log.access.add);
 
 app.get('/log/usage', (req, res)=>{ res.send(log.usage.get()) }); 
 app.get('/log/access', (req, res)=>{ res.send(log.access.get()) }); 
