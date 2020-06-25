e7cfc681bd688845820dca76a6f15badc0492399
taiga39 <ishikawa.tigers@gmail.com>
2020-06-26 01:41:41 +0900

    redis登録

--- server.js
+++ server.js
@@ -4,6 +4,26 @@ var http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 7000;
 
+
+const redis = require('redis')
+// 接続情報
+const config = {
+    host: '127.0.0.1',
+    port: 6379
+}
+// 接続
+const client = redis.createClient(config)
+// データの登録
+client.set('key', 'value')
+
+// データの取得と表示
+client.get('key', (err, reply) => {
+  console.log(reply)
+})
+// 切断
+client.quit()
+
+
 app.get('/' , function(req, res){
     res.sendFile(__dirname+'/public/index.html');
 });
