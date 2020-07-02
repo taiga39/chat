c2909d7c58c98d45ad080d1a57a38e2622875bf3
taiga39 <ishikawa.tigers@gmail.com>
2020-07-02 23:20:16 +0900

    修正

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ io.on('connection',function(socket){
   });
   socket.on('room',function(msg){
     socket.join('testroom')
-    io.to("testroom").emit('chat', "入室したよん");
+    io.to("testroom").emit('chat', "入室したよんさ");
 });
   socket.on('disconnect', () => {
     console.log('user disconnected');
